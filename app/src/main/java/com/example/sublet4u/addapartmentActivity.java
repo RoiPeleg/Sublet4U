bea21c258b303a92436d2812df682e995ee569ad
Roi <roi2307@gmail.com>
2020-11-25 19:52:10 +0200

    added photo addition partly

--- app/src/main/java/com/example/sublet4u/addapartmentActivity.java
+++ app/src/main/java/com/example/sublet4u/addapartmentActivity.java
@@ -1,6 +1,11 @@
 package com.example.sublet4u;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.sublet4u.data.model.Aparetment;
@@ -10,21 +15,28 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class addapartmentActivity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class addapartmentActivity extends AppCompatActivity {
+     ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addapartment);
         final Button done = findViewById(R.id.done);
+        final Button addP = findViewById(R.id.photos);
         final EditText name = findViewById(R.id.Name);
         final EditText desc = findViewById(R.id.description);
         final EditText address = findViewById(R.id.address);
+        imageView = findViewById(R.id.imageView);
         FirebaseAuth mAuth;
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("");
@@ -37,5 +49,35 @@ public class addapartmentActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        addP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED) {
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+        }
+    }
     }
 }
