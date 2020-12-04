fe0d5b3d0f4ddc6ea320249a54a6240a5cb2c3c4
hezzi123 <47628021+hezzi123@users.noreply.github.com>
2020-12-04 20:37:35 +0200

    client profile

---
+++ app/src/main/java/com/example/sublet4u/data/model/Client.java
@@ -0,0 +1,42 @@
+package com.example.sublet4u.data.model;
+
+import android.widget.ImageView;
+
+public class Client
+{
+    public String name;
+    public String desc;
+    public String clientID;
+//    public ImageView imageView;
+    public String sex;
+
+    public Client(){
+        this.name = "";
+        this.desc = "";
+        this.clientID = "";
+    }
+
+    public Client(String name1, String desc, String clientID, String sex){
+        this.name = name1;
+        this.desc = desc;
+        this.clientID = clientID;
+//        this.imageView  = imageView;
+        this.sex = sex;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getClientID() {
+        return clientID;
+    }
+}
