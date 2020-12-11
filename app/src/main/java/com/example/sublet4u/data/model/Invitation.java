0c8a59c8fa0ffb8f0c11308d093525197fa61605
TalAbed <talabed46@gmail.com>
2020-12-11 11:59:50 +0200

    add dates

--- app/src/main/java/com/example/sublet4u/data/model/Invitation.java
+++ app/src/main/java/com/example/sublet4u/data/model/Invitation.java
@@ -4,15 +4,21 @@ public class Invitation {
 
     public String apartmentID;
     public String clientID;
+    public String arriveDate;
+    public String leaveDate;
 
     public Invitation(){
         this.apartmentID="";
         this.clientID="";
+        this.arriveDate="";
+        this.leaveDate="";
     }
 
-    public  Invitation (String apartmentID, String clientID){
+    public  Invitation (String apartmentID, String clientID, String arriveDate, String leaveDate){
         this.apartmentID = apartmentID;
         this.clientID = clientID;
+        this.arriveDate = arriveDate;
+        this.leaveDate = leaveDate;
     }
 
     public String getClientID()
