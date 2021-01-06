6049d6411e6077bbc19cb221a9af65d4b3a5089c
runeterra <ariking5@gmail.com>
2021-01-06 07:08:38 +0200

    synchronization of invitations

--- app/src/main/java/com/example/sublet4u/customer/FindApartmentUser.java
+++ app/src/main/java/com/example/sublet4u/customer/FindApartmentUser.java
@@ -21,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.sublet4u.R;
+import com.example.sublet4u.data.model.Respond;
 import com.example.sublet4u.owner.WatchReviewsActivity;
 import com.example.sublet4u.data.model.Apartment;
 import com.example.sublet4u.data.model.Invitation;
@@ -38,6 +39,7 @@ import com.google.firebase.database.collection.LLRBNode;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -45,6 +47,7 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class FindApartmentUser extends AppCompatActivity {
     DatabaseReference reference;
@@ -82,7 +85,6 @@ public class FindApartmentUser extends AppCompatActivity {
                 int year = cal.get(Calendar.YEAR);
                 int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
-
                 DatePickerDialog dialog = new DatePickerDialog(FindApartmentUser.this,
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener, year, month, day);
 
@@ -95,9 +97,6 @@ public class FindApartmentUser extends AppCompatActivity {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int day)
             {
-
-
-
                 String int_day_tostring = "" + day;
                 String int_month_tostring = ""+ (month+1);
                 String date ="";
@@ -115,14 +114,92 @@ public class FindApartmentUser extends AppCompatActivity {
                     cal.add(Calendar.DATE, -1);
                     Date date1 = (cal.getTime());
                     Date date2 = sdf.parse(date);
-                    if (date1.compareTo(date2) > 0)
+                    if (!showTheLeaveDate.getText().equals("Set here your leave date"))
                     {
-                        Toast.makeText(getApplicationContext(), "The arrive date is before than the current date", Toast.LENGTH_LONG).show();
+                        Date date5 = sdf.parse(showTheLeaveDate.getText().toString());
+                        if (date2.compareTo(date5) > 0)
+                        {
+                            Toast.makeText(getApplicationContext(), "The arrive date is later than the leave date", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            showTheArriveDate.setText(date);
+//                            Toast.makeText(getApplicationContext(), showTheArriveDate.getText().toString(), Toast.LENGTH_LONG).show();
+                            arriveDate = (date);
+                        }
                     }
                     else
                     {
-                        showTheArriveDate.setText(date);
-                        arriveDate = (date);
+                        myRef.child("Invitations").orderByChild("apartmentID").equalTo(apartID).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot)
+                            {
+                                for (DataSnapshot s : snapshot.getChildren())
+                                {
+                                    Invitation invitation = s.getValue(Invitation.class);
+                                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ITALY);
+                                    Date date1 = null;
+                                    Date date2 = null;
+                                    Date date3 = null;
+                                    try {
+                                        date1 = sdf.parse(showTheArriveDate.getText().toString());
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        assert invitation != null;
+                                        date2 = sdf.parse(invitation.arriveDate);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        date3 = sdf.parse(invitation.leaveDate);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    if ((date1.compareTo(date2) >= 0 && date1.compareTo(date3) <= 0))
+                                    {
+                                        myRef.child("Responds").orderByChild("invitationID").equalTo(s.getKey()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot)
+                                            {
+                                                for (DataSnapshot ss : snapshot.getChildren())
+                                                {
+                                                    Respond respond = ss.getValue(Respond.class);
+                                                    if (respond.status)
+                                                    {
+                                                        Toast.makeText(getApplicationContext(), "This date already caught up between:", Toast.LENGTH_LONG).show();
+                                                        Toast.makeText(getApplicationContext(),invitation.arriveDate + "-" + invitation.leaveDate , Toast.LENGTH_LONG).show();
+                                                        showTheArriveDate.setText("Set here your arrive date");
+                                                    }
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                            }
+                                        });
+
+                                    }
+                                }
+
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                        if (date1.compareTo(date2) > 0)
+                        {
+                            Toast.makeText(getApplicationContext(), "The arrive date is before than the current date", Toast.LENGTH_LONG).show();
+                        }
+                        else
+                        {
+                            showTheArriveDate.setText(date);
+//                            Toast.makeText(getApplicationContext(), showTheArriveDate.getText().toString(), Toast.LENGTH_LONG).show();
+                            arriveDate = (date);
+                        }
                     }
                 }catch (Exception e) {
                     e.printStackTrace();
@@ -163,22 +240,95 @@ public class FindApartmentUser extends AppCompatActivity {
                 else
                     date = date+(month+1)+"/"+year;
                 try {
-                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.US);
-                    Date date1 = sdf.parse(arriveDate);
-                    Date date2 = sdf.parse(date);
-                    if (date1.compareTo(date2) > 0)
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ITALY);
+                    if (showTheArriveDate.getText().equals("Set here your arrive date"))
                     {
-                        Toast.makeText(getApplicationContext(), "The arrive date is later than the leave date", Toast.LENGTH_LONG).show();
-                    }
-                    else if (date1.compareTo(date2) == 0)
-                    {
-                        showTheLeaveDate.setText(date);
-                        leaveDate = date;
+                        Toast.makeText(getApplicationContext(), "Choose your arrive date first", Toast.LENGTH_LONG).show();
                     }
                     else
                     {
-                        showTheLeaveDate.setText(date);
-                        leaveDate = date;
+                        Date date1 = sdf.parse(arriveDate);
+                        Date date2 = sdf.parse(date);
+                        myRef.child("Invitations").orderByChild("apartmentID").equalTo(apartID).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot)
+                            {
+                                for (DataSnapshot s : snapshot.getChildren())
+                                {
+                                    Invitation invitation = s.getValue(Invitation.class);
+                                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.ITALY);
+                                    Date date = null;
+                                    Date date1 = null;
+                                    Date date2 = null;
+                                    Date date3 = null;
+                                    try {
+                                        date = sdf.parse(showTheArriveDate.getText().toString());
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        date1 = sdf.parse(showTheLeaveDate.getText().toString());
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        assert invitation != null;
+                                        date2 = sdf.parse(invitation.arriveDate);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        date3 = sdf.parse(invitation.leaveDate);
+                                    } catch (ParseException e) {
+                                        e.printStackTrace();
+                                    }
+                                    if ((date1.compareTo(date2) >= 0 && date1.compareTo(date3) <= 0) || ((date2.compareTo(date) >= 0 && date2.compareTo(date1) <= 0)
+                                    && (date3.compareTo(date) >= 0 && date3.compareTo(date1) <= 0)))
+                                    {
+                                        myRef.child("Responds").orderByChild("invitationID").equalTo(s.getKey()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot)
+                                            {
+                                                for (DataSnapshot ss : snapshot.getChildren())
+                                                {
+                                                    Respond respond = ss.getValue(Respond.class);
+                                                    if (respond.status)
+                                                    {
+                                                        Toast.makeText(getApplicationContext(), "This date already caught up between:", Toast.LENGTH_LONG).show();
+                                                        Toast.makeText(getApplicationContext(),invitation.arriveDate + "-" + invitation.leaveDate , Toast.LENGTH_LONG).show();
+                                                        showTheLeaveDate.setText("Set here your arrive date");
+                                                    }
+                                                }
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+
+                                            }
+                                        });
+                                    }
+                                }
+
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                        if (date1.compareTo(date2) > 0)
+                        {
+                            Toast.makeText(getApplicationContext(), "The arrive date is later than the leave date", Toast.LENGTH_LONG).show();
+                        }
+                        else if (date1.compareTo(date2) == 0)
+                        {
+                            showTheLeaveDate.setText(date);
+                            leaveDate = date;
+                        }
+                        else
+                        {
+                            showTheLeaveDate.setText(date);
+                            leaveDate = date;
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -306,15 +456,15 @@ public class FindApartmentUser extends AppCompatActivity {
         final Button dislike = findViewById(R.id.dislike);
 
         Iterator<String> appartmentsIterator = apartments.iterator();
+
         apartID = appartmentsIterator.next();
         loadData(apartID);
-
         like.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v){
-                if(!(showTheArriveDate.getText().equals("Set here your arrive date")) && !(showTheLeaveDate.getText().equals("Set here your leave date"))) {
+                if(!(showTheArriveDate.getText().equals("Set here your arrive date")) && !(showTheLeaveDate.getText().equals("Set here your leave date")))
+                {
                     String invitation_id = myRef.child("Invitations").push().getKey();
-                    System.out.println(apartID);
                     myRef.child("Invitations").child(invitation_id).setValue(new Invitation(apartID, mAuth.getUid(), arriveDate, leaveDate, p, mAuth.getCurrentUser().getDisplayName(), apart.name));
                     if (appartmentsIterator.hasNext()) {
                         apartID = appartmentsIterator.next();
